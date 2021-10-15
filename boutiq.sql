1976ae104a6cbbb025ff7fefa9f449b7c2e6d660
ViciousCarpet <soudierkilian@gmail.com>
2021-10-15 15:07:25 +0200

    Bonne BDD

--- boutiq.sql
+++ boutiq.sql
@@ -1,185 +1,143 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le : lun. 24 mai 2021 à 12:22
--- Version du serveur :  8.0.21
--- Version de PHP : 7.3.21
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- --------------------------------------------------------
+-- Hôte:                         127.0.0.1
+-- Version du serveur:           5.7.33 - MySQL Community Server (GPL)
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             11.2.0.6213
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Base de données : `boutiq`
---
 
--- --------------------------------------------------------
+-- Listage de la structure de la base pour ecommerce
+CREATE DATABASE IF NOT EXISTS `ecommerce` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `ecommerce`;
 
---
--- Structure de la table `categorie`
---
-
-DROP TABLE IF EXISTS `categorie`;
+-- Listage de la structure de la table ecommerce. categorie
 CREATE TABLE IF NOT EXISTS `categorie` (
-  `idCategorie` int NOT NULL AUTO_INCREMENT,
-  `nomCategorie` varchar(500) NOT NULL,
+  `idCategorie` int(11) NOT NULL AUTO_INCREMENT,
+  `nomCategorie` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`idCategorie`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
---
--- Déchargement des données de la table `categorie`
---
-
+-- Listage des données de la table ecommerce.categorie : ~3 rows (environ)
+/*!40000 ALTER TABLE `categorie` DISABLE KEYS */;
 INSERT INTO `categorie` (`idCategorie`, `nomCategorie`) VALUES
-(1, 'Jeux vidéo & consoles'),
-(2, 'Informatique & bureau'),
-(3, 'Livres'),
-(4, 'Musique, DVD & Blu-ray');
+	(1, 'Accessoires'),
+	(2, 'Instruments'),
+	(5, 'Montage');
+/*!40000 ALTER TABLE `categorie` ENABLE KEYS */;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `client`
---
-
-DROP TABLE IF EXISTS `client`;
+-- Listage de la structure de la table ecommerce. client
 CREATE TABLE IF NOT EXISTS `client` (
-  `idClient` int NOT NULL AUTO_INCREMENT,
-  `nomClient` varchar(500) NOT NULL,
-  `prenomClient` varchar(500) NOT NULL,
-  `emailClient` varchar(500) NOT NULL,
-  `motDePasseClient` varchar(1000) NOT NULL,
-  `rueClient` varchar(1000) NOT NULL,
-  `cpClient` varchar(10) NOT NULL,
-  `villeClient` varchar(500) NOT NULL,
+  `idClient` int(11) NOT NULL AUTO_INCREMENT,
+  `nomClient` varchar(50) DEFAULT NULL,
+  `prenomClient` varchar(50) DEFAULT NULL,
+  `emailClient` varchar(50) DEFAULT NULL,
+  `motDePasseClient` varchar(500) DEFAULT NULL,
+  `rueClient` varchar(50) DEFAULT NULL,
+  `cpClient` varchar(5) DEFAULT NULL,
+  `villeClient` varchar(50) DEFAULT NULL,
   `telClient` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`idClient`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
---
--- Déchargement des données de la table `client`
---
-
+-- Listage des données de la table ecommerce.client : ~4 rows (environ)
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
 INSERT INTO `client` (`idClient`, `nomClient`, `prenomClient`, `emailClient`, `motDePasseClient`, `rueClient`, `cpClient`, `villeClient`, `telClient`) VALUES
-(3, 'Lucas', 'Chevalot', 'lucas.chevalot@gmail.com', '$2y$10$qJZGoizlRObBHtcfHlNztOwjSumUsSqmJYAf.bKqIWzCq6W7VVp6C', '15 rue du gué', '55000', 'Bar le duc', '0648789090');
+	(3, 'Lucas', 'Chevalot', 'lucas.chevalot@gmail.com', '$2y$10$qJZGoizlRObBHtcfHlNztOwjSumUsSqmJYAf.bKqIWzCq6W7VVp6C', '15 rue du gué', '55000', 'Bar le duc', '0648789090'),
+	(4, 'blabla', 'bla', 'mdflsdfm@exemple.com', '$2y$10$28TnXAYin9NGjYVSmOVqtOMcoJI/nnVjbFlQBTM5TUl8jvsbSMMWu', 'msdlf;', '12345', 'zrmlg;z', ''),
+	(5, 'knsd', 'lsdnfm', 'lskfdlq@lsmdf.com', '$2y$10$rAuGNSyumCGRbh5SODG9Neeli4uYs2UWpIVhfD/26mMSTNpeoWPUq', 'klfdj', '45678', 'kfeajln', ''),
+	(6, 'sksl', 'lsfdk', 'soudierkilian@gmail.com', '$2y$10$9d8kquzMvaHJ1xMYIX5GjeLF4U/vlczYzO/z9kpRqJ7iRkKWSY6wK', 'zdklfj', '79954', 'lsdpfkm', '');
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `commande`
---
-
-DROP TABLE IF EXISTS `commande`;
+-- Listage de la structure de la table ecommerce. commande
 CREATE TABLE IF NOT EXISTS `commande` (
-  `numeroCommande` int NOT NULL AUTO_INCREMENT,
-  `dateCommande` date NOT NULL,
-  `idClient` int NOT NULL,
+  `numeroCommande` int(11) NOT NULL AUTO_INCREMENT,
+  `dateCommande` date DEFAULT NULL,
+  `idClient` int(11) DEFAULT NULL,
+  `devis` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`numeroCommande`),
-  KEY `commande_client_FK` (`idClient`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+  KEY `FK_commande_client` (`idClient`),
+  CONSTRAINT `FK_commande_client` FOREIGN KEY (`idClient`) REFERENCES `client` (`idClient`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
---
--- Déchargement des données de la table `commande`
---
+-- Listage des données de la table ecommerce.commande : ~0 rows (environ)
+/*!40000 ALTER TABLE `commande` DISABLE KEYS */;
+INSERT INTO `commande` (`numeroCommande`, `dateCommande`, `idClient`, `devis`) VALUES
+	(19, '2021-09-27', 4, 0),
+	(20, '2021-10-15', 6, 1);
+/*!40000 ALTER TABLE `commande` ENABLE KEYS */;
 
-INSERT INTO `commande` (`numeroCommande`, `dateCommande`, `idClient`) VALUES
-(15, '2020-11-13', 3),
-(16, '2020-11-13', 3),
-(17, '2020-11-13', 3);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `commander`
---
-
-DROP TABLE IF EXISTS `commander`;
+-- Listage de la structure de la table ecommerce. commander
 CREATE TABLE IF NOT EXISTS `commander` (
-  `numeroCommande` int NOT NULL,
-  `codeProduit` int NOT NULL,
-  `quantite` int NOT NULL,
-  PRIMARY KEY (`numeroCommande`,`codeProduit`),
-  KEY `commander_produit0_FK` (`codeProduit`)
+  `numeroCommande` int(11) NOT NULL DEFAULT '0',
+  `codeProduit` int(11) NOT NULL DEFAULT '0',
+  `quantite` int(11) NOT NULL DEFAULT '0',
+  KEY `FK_commander_commande` (`numeroCommande`),
+  KEY `FK_commander_produit` (`codeProduit`),
+  CONSTRAINT `FK_commander_commande` FOREIGN KEY (`numeroCommande`) REFERENCES `commande` (`numeroCommande`),
+  CONSTRAINT `FK_commander_produit` FOREIGN KEY (`codeProduit`) REFERENCES `produit` (`codeProduit`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Déchargement des données de la table `commander`
---
-
+-- Listage des données de la table ecommerce.commander : ~2 rows (environ)
+/*!40000 ALTER TABLE `commander` DISABLE KEYS */;
 INSERT INTO `commander` (`numeroCommande`, `codeProduit`, `quantite`) VALUES
-(15, 7, 1),
-(15, 8, 2),
-(16, 9, 1),
-(16, 10, 1),
-(17, 4, 2);
+	(19, 13, 1),
+	(19, 15, 1);
+/*!40000 ALTER TABLE `commander` ENABLE KEYS */;
 
--- --------------------------------------------------------
+-- Listage de la structure de la table ecommerce. photo
+CREATE TABLE IF NOT EXISTS `photo` (
+  `id_photo` int(11) NOT NULL AUTO_INCREMENT,
+  `photos` varchar(50) DEFAULT NULL,
+  `id_produit` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_photo`),
+  KEY `FK_photo_produit` (`id_produit`),
+  CONSTRAINT `FK_photo_produit` FOREIGN KEY (`id_produit`) REFERENCES `produit` (`codeProduit`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
---
--- Structure de la table `produit`
---
+-- Listage des données de la table ecommerce.photo : ~8 rows (environ)
+/*!40000 ALTER TABLE `photo` DISABLE KEYS */;
+INSERT INTO `photo` (`id_photo`, `photos`, `id_produit`) VALUES
+	(1, 'produit1.jpg', 13),
+	(2, 'produit2.jpg', 13),
+	(3, 'produit3.jpg', 15),
+	(4, 'produit4.jpg', 15),
+	(5, 'produit5.jpg', 17),
+	(6, 'produit6.jpg', 18),
+	(7, 'produit7.jpg', 18),
+	(8, 'sur_devis.jpg', 20);
+/*!40000 ALTER TABLE `photo` ENABLE KEYS */;
 
-DROP TABLE IF EXISTS `produit`;
+-- Listage de la structure de la table ecommerce. produit
 CREATE TABLE IF NOT EXISTS `produit` (
-  `codeProduit` int NOT NULL AUTO_INCREMENT,
-  `designationProduit` varchar(500) NOT NULL,
-  `prixProduit` decimal(10,2) NOT NULL,
-  `stockProduit` int NOT NULL,
-  `photoProduit` varchar(1000) NOT NULL,
-  `idCategorie` int NOT NULL,
+  `codeProduit` int(11) NOT NULL AUTO_INCREMENT,
+  `designationProduit` varchar(50) NOT NULL DEFAULT '0',
+  `prixProduit` double(10,2) NOT NULL DEFAULT '0.00',
+  `stockProduit` int(11) NOT NULL DEFAULT '0',
+  `idCategorie` int(11) NOT NULL DEFAULT '0',
+  `typeProduit` varchar(50) NOT NULL DEFAULT '0',
   PRIMARY KEY (`codeProduit`),
-  KEY `produit_categorie_FK` (`idCategorie`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+  KEY `FK_produit_categorie` (`idCategorie`),
+  CONSTRAINT `FK_produit_categorie` FOREIGN KEY (`idCategorie`) REFERENCES `categorie` (`idCategorie`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
---
--- Déchargement des données de la table `produit`
---
+-- Listage des données de la table ecommerce.produit : ~5 rows (environ)
+/*!40000 ALTER TABLE `produit` DISABLE KEYS */;
+INSERT INTO `produit` (`codeProduit`, `designationProduit`, `prixProduit`, `stockProduit`, `idCategorie`, `typeProduit`) VALUES
+	(13, 'Guitare électrique', 854.68, 1, 2, 'Prix fixe'),
+	(15, 'Guitare électrique deux manches', 256.87, 3, 2, 'Prix fixe'),
+	(17, 'Cordes guitare 10-46', 10.00, 20, 1, 'Prix fixe'),
+	(18, 'Cordes basse fluo', 50.00, 10, 1, 'Prix fixe'),
+	(20, 'Montage des cordes', 0.00, 0, 5, 'Sur devis');
+/*!40000 ALTER TABLE `produit` ENABLE KEYS */;
 
-INSERT INTO `produit` (`codeProduit`, `designationProduit`, `prixProduit`, `stockProduit`, `photoProduit`, `idCategorie`) VALUES
-(1, 'Skidamarink (Guillaume Musso)', '19.90', 45, '71aaz7oUpBL._AC._SR360,460.jpg', 3),
-(2, 'Le Crépuscule et l\'Aube (Ken FOLLETT)', '24.50', 35, '514fHMab5eL._SX316_BO1,204,203,200_.jpg', 3),
-(3, 'D\'un monde à l\'autre: Le temps des consciences (de Frédéric Lenoir, Nicolas Hulot)', '21.50', 64, '41vbzQVGIOL._SX311_BO1,204,203,200_.jpg', 3),
-(4, 'SanDisk Carte Mémoire MicroSDHC Ultra 128 Go + Adaptateur SD Classe 10, U1, Homologuée A1', '30.39', 195, '617NtexaW2L._AC_SL1500_.jpg', 2),
-(5, 'Logitech Webcam C920 HD Pro, Appels et Enregistrements Vidéo Full HD 1080p, Gaming Stream, Deux Microphones, Petite, Agile, Réglable, Noir', '109.00', 32, '51r+t90LYxL._AC_SL1023_.jpg', 2),
-(6, 'Samsung SSD Interne 860 EVO 2.5\" (500 Go) - MZ-76E500B/EU', '70.12', 254, '91JA5-hAnoL._AC_SL1500_.jpg', 2),
-(7, 'Corsair HS35 Casque de Gaming Stéréo', '43.65', 20, '619v-GUF7tL._AC._SR360,460.jpg', 1),
-(8, 'Razer Wolverine Ultimate for Xbox One - Manette de Jeu pour Xbox One ', '179.99', 235, '618f9UKaVmL._AC._SR360,460.jpg', 1),
-(9, 'Sign O’ The Times-Deluxe Edition 3CD', '19.99', 15, 'A1bbWtAHUtL._AC._SR360,460.jpg', 4),
-(10, 'Batman - The Dark Knight, le Chevalier Noir - 4K Ultra HD', '20.00', 164, '81jSFtA8LOL._AC._SR360,460.jpg', 4),
-(11, 'Game Of Thrones (Le Trône de Fer) - Saison 8', '39.99', 98, '71-we6++h2L._AC._SR360,460.jpg', 4);
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `commande`
---
-ALTER TABLE `commande`
-  ADD CONSTRAINT `commande_client_FK` FOREIGN KEY (`idClient`) REFERENCES `client` (`idClient`);
-
---
--- Contraintes pour la table `commander`
---
-ALTER TABLE `commander`
-  ADD CONSTRAINT `commander_commande_FK` FOREIGN KEY (`numeroCommande`) REFERENCES `commande` (`numeroCommande`),
-  ADD CONSTRAINT `commander_produit0_FK` FOREIGN KEY (`codeProduit`) REFERENCES `produit` (`codeProduit`);
-
---
--- Contraintes pour la table `produit`
---
-ALTER TABLE `produit`
-  ADD CONSTRAINT `produit_categorie_FK` FOREIGN KEY (`idCategorie`) REFERENCES `categorie` (`idCategorie`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
